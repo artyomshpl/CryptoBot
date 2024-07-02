5dfca267bdf0e44cfc65581940b56fe2d38986bf
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-07-03 02:36:03 +0300

    Controllers app initialized

---
+++ src/main/java/com/shep/cryptobot/Controller/Controller.java
@@ -0,0 +1,30 @@
+package com.shep.cryptobot.Controller;
+
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+public class Controller {
+
+    private final DiscoveryClient discoveryClient;
+    private final RestClient restClient;
+
+    public Controller(DiscoveryClient discoveryClient, RestClient.Builder restClientBuilder) {
+        this.discoveryClient = discoveryClient;
+        restClient = restClientBuilder.build();
+    }
+
+    @GetMapping("helloEureka")
+    public String helloWorld() {
+        ServiceInstance serviceInstance = discoveryClient.getInstances("CryptoBotServices").get(0);
+        String serviceAResponse = restClient.get()
+                .uri(serviceInstance.getUri() + "/helloWorld")
+                .retrieve()
+                .body(String.class);
+        return serviceAResponse;
+    }
+}
